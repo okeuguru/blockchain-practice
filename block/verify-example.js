76e39479b0c06f92eaa0baa587094dd895d26f0d
Oke Uguru <ondukwe@gmail.com>
2019-03-26 17:09:13 -0400

    added block explorer

--- block/verify-example.js
+++ block/verify-example.js
@@ -4,10 +4,10 @@ const bitcoinMessage = require('bitcoinjs-message')
 
 
 // Verify a Bitcoin message
-const address = '1HZwkjkeaoZfTSaJxDw6aKkxp45agDiEzN'
+const address = 'bc1qc4dejd8880r3txelpaw5yzqzxtl2h32sm50dkz'
 
 const signature = 'HJLQlDWLyb1Ef8bQKEISzFbDAKctIlaqOpGbrk3YVtRsjmC61lpE5ErkPRUFtDKtx98vHFGUWlFhsh3DiW6N0rE'
 
-const message = 'This is an example of a signed message.'
+const message = 'Udacity rocks!'
 
 console.log(bitcoinMessage.verify(message, address, signature));
